bf1ee3e014beae1064610e6816d46d4efaf83e8d
Rahim Khoja <rahim@khoja.ca>
2024-06-09 16:41:13 -0700

    Update context.py

--- tests/context.py
+++ tests/context.py
@@ -3,11 +3,4 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import cudf
-import pandas_taoverrides = pandas_ta.OVERRIDES
-pandas_ta.OVERRIDES = overrides
-cudf.core.DataFrame.ta = lambda df, *args, **kwargs: pandas_ta_compute(df, *args, **kwargs)
-def pandas_ta_compute(df, *args, **kwargs):
-    pdf = df.to_pandas()
-    result = getattr(pandas_ta, df.ta.__name__)(*args, **kwargs)(pdf)
-    return cudf.from_pandas(result)
+import pandas_ta

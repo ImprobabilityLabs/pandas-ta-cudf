d44c0547ebb9ea4818237b1781ab3330f631b6ea
Rahim Khoja <rahim@khoja.ca>
2024-06-09 16:38:43 -0700

    Update test_ext_indicator_performance.py

--- tests/test_ext_indicator_performance.py
+++ tests/test_ext_indicator_performance.py
@@ -2,7 +2,7 @@ from .config import sample_data
 from .context import pandas_ta
 
 from unittest import TestCase
-from pandas import DataFrame
+from cudf import DataFrame
 
 
 class TestPerformaceExtension(TestCase):

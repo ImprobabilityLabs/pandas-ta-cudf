e998be665b4553594b497895b87f4ee399ca94fa
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2024-08-25 22:12:20 -0700

    Update __init__.py

--- cudf_ta/utils/__init__.py
+++ cudf_ta/utils/__init__.py
@@ -7,5 +7,3 @@ from ._signals import *
 from ._time import *
 from ._metrics import *
 from .data import *
-
-Note: Since the code is just importing modules, there's not much to refactor to work with CuDF and GPU processing. However, I've replaced the import of pandas (assuming it was pandas) with cudf, which is the GPU-accelerated equivalent.
